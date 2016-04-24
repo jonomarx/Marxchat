8aff06b8efb046b7c691746fdd06a2e9974710c1
Dan Marx <marx.dan@gmail.com>
2016-04-23 17:21:11 -0700

    latest

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, Response, jsonify
 import time, uuid, sys
 
+# Initialize Mongo
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client.messageDB
+collection = db.message
+
+# Main 
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,17 +18,18 @@ def homepage():
 
 def squeek_message(username):
     if request.method == 'POST':
-        msg_to=request.form['to']
-        msg_from=request.form['from']
-        msg=request.form['msg']
-        raise Exception(msg_to, msg_from, msg)
-        # print("hello world", file=sys.stderr)
-        # return jsonify({'status': 'This is a POST message'})
+        write(request.json)
+        return 
+        # return jsonify({'Message ID': 'OK'})
+    
     elif request.method == 'GET':
         return jsonify({'messages': [{'id':uuid.uuid4(), 'from':'dad', 'date':time.time(), 'msg': 'This is a test'}, {'id':uuid.uuid4(), 'from':'dad', 'date':time.time(), 'msg': 'test'}]})  
 
-
-
+def write(post):
+    posts = db.posts
+    post_id = posts.insert_one(post).inserted_id
+    post_id
+    return post_id
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int("80"), debug=True)
