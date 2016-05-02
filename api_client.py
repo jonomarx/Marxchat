3e9334a71600ffdeeae457cfff813a924fbe1b21
jonomarx <jon.marx@me.com>
2016-05-02 16:18:05 -0700

    more user friendly client

--- api_client.py
+++ api_client.py
@@ -7,3 +7,14 @@ def POST(url,data,username=None):
     handler = urllib.request.urlopen( url, data )
     print("you send: ")
     print( handler.read().decode( 'utf-8' ) )
+
+while True:
+    response = input("type get to get messages or type send to send a message.")
+    if response == 'get':
+        print(GET(("localhost/message/"+user_name)))
+    elif response == 'send':
+        data = {}
+        for i in ["to","from","msg"]:
+            response = input(i)
+            data[i] = response
+        POST(("localhost/message/"+user_name))
